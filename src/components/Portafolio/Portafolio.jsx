f79e697b1dced6a13131bab74ec0b6dff47e6b5f
danielSV <danielsilvavalencia25@gmail.com>
2026-01-30 12:09:08 -0500

    parte portafolio proyectos en proceso

---
+++ src/components/Portafolio/Portafolio.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Portafolio.css";
+import "./MediaQueryPorta.css";
+import image1 from "../../assets/img/img-proyect/1.PNG";
+import image2 from "../../assets/img/img-proyect/2.PNG";
+import image3 from "../../assets/img/img-proyect/3.PNG";
+import image4 from "../../assets/img/img-proyect/4.PNG";
+
+const portafolioItems = [
+  {
+    id: 1,
+    image: "WAPSI",
+    description:
+      "Wapsi - Servicios Integrales, desarrollado con JS & CSS, NodeJS",
+    demonLink: "https://proyect-practice.vercel.app/",
+  },
+  {
+    id: 2,
+    image: "Tienda Virtual - Autopartes",
+    description:
+      "Desarrollado con React-CSS",
+    demonLink: "https://proyecto-react-autopartes.vercel.app/",
+  },
+  {
+    id: 3,
+    image: "Biblioteca Virtual de cursos Online",
+    description:
+      "Desarrollado con JS & CSS, NodeJS",
+    demonLink: "https://tienda-cursos-mu.vercel.app/",
+  },
+  {
+    id: 4,
+    image: "Github Search",
+    description:
+      "Desarrollado con React Material-UI",
+    demonLink: "",
+  },
+];
+
+export default function Portafolio() {
+  return (
+    <div className="">
+      <div className=""></div>
+    </div>
+  );
+}
